d172961704a0ca9ab142e0b9b5bf9d81a2f8ae75
Michael Patterson <mjpatter88@gmail.com>
2021-09-15 08:39:47 -0500

    Add support for summing tails of a number of sequences.

--- arrays/sum.go
+++ arrays/sum.go
@@ -2,8 +2,8 @@ package main
 
 func Sum(numbers []int) int {
 	sum := 0
-	for _, number := range numbers{
-	  sum += number
+	for _, number := range numbers {
+		sum += number
 	}
 	return sum
 }
@@ -13,5 +13,19 @@ func SumAll(numbersToSum ...[]int) []int {
 	for _, numbers := range numbersToSum {
 		sums = append(sums, Sum(numbers))
 	}
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
 	return sums
 }
