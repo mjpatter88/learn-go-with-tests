2f0405b38c33f118f174c8b5df8480144eb59789
Michael Patterson <mjpatter88@gmail.com>
2021-09-19 21:18:52 -0500

    Add support for adding a word to the dictionary.

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -19,6 +19,27 @@ func TestSearch(t *testing.T) {
 	})
 }
 
+func TestAdd(t *testing.T) {
+	t.Run("new word", func(t *testing.T) {
+		dictionary := Dictionary{}
+		word := "test"
+		definition := "this is just a test"
+
+		dictionary.Add(word, definition)
+		assertDefintion(t, dictionary, word, definition)
+	})
+
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+		err := dictionary.Add(word, "new test")
+
+		assertError(t, err, ErrWordExists)
+		assertDefintion(t, dictionary, word, definition)
+	})
+}
+
 func assertStrings(t testing.TB, got string, want string) {
 	t.Helper()
 	if got != want {
@@ -30,5 +51,17 @@ func assertError(t testing.TB, got error, want error) {
 	t.Helper()
 	if got != want {
 		t.Errorf("got error %q want error %q", got, want)
+	}
+}
+
+func assertDefintion(t testing.TB, dictionary Dictionary, word string, definition string) {
+	t.Helper()
+
+	got, err := dictionary.Search(word)
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+	if got != definition {
+		t.Errorf("got %s want %s", got, definition)
 	}
 }
