c02902c4ab45212a37b64d021ac3ea36283a5bb8
Michael Patterson <mjpatter88@gmail.com>
2021-09-23 19:49:23 -0500

    Sleep for one second each step of the countdown.

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -3,6 +3,7 @@ package mocking
 import (
 	"fmt"
 	"io"
+	"time"
 )
 
 const finalWord = "Go!"
@@ -11,6 +12,7 @@ const countdownStart = 3
 func Countdown(out io.Writer) {
 	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
+		time.Sleep(1 * time.Second)
 	}
 	fmt.Fprintf(out, finalWord)
 }
