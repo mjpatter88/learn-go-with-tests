80bc4dc9a478a0ff732815edd757854aecaf8916
Michael Patterson <mjpatter88@gmail.com>
2021-09-23 19:47:14 -0500

    Count down from 3 to 0 and then print Go!.

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -5,7 +5,12 @@ import (
 	"io"
 )
 
-func Countdown(out io.Writer) {
-	fmt.Fprintf(out, "3")
+const finalWord = "Go!"
+const countdownStart = 3
 
+func Countdown(out io.Writer) {
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+	}
+	fmt.Fprintf(out, finalWord)
 }
