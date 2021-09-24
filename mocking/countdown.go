6ab6d784579d5caaa21be5f9d29cac34412f6fd3
Michael Patterson <mjpatter88@gmail.com>
2021-09-23 19:59:54 -0500

    Define a sleeper interface so we can test it more easily.

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -3,16 +3,20 @@ package mocking
 import (
 	"fmt"
 	"io"
-	"time"
 )
+
+type Sleeper interface {
+	Sleep()
+}
 
 const finalWord = "Go!"
 const countdownStart = 3
 
-func Countdown(out io.Writer) {
+func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
+		sleeper.Sleep()
 		fmt.Fprintln(out, i)
-		time.Sleep(1 * time.Second)
 	}
+	sleeper.Sleep()
 	fmt.Fprintf(out, finalWord)
 }
